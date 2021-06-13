1e6a6ad63ed910edd91f72531fedbdd1d2743b9a
Dominic Schmidt <dev@abstraqt.de>
2021-06-13 15:44:36 +0200

    Initial commit

---
+++ Package.swift
@@ -0,0 +1,24 @@
+// swift-tools-version:5.3
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "Owl",
+	products: [
+		.executable(name: "owl", targets: ["Owl"])
+	],
+    dependencies: [
+		.package(url: "https://github.com/apple/swift-argument-parser", from: "0.4.0"),
+    ],
+    targets: [
+        .target(
+            name: "Owl",
+            dependencies: [
+				.product(name: "ArgumentParser", package: "swift-argument-parser"),
+			]),
+        .testTarget(
+            name: "OwlTests",
+            dependencies: ["Owl"]),
+    ]
+)
